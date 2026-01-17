0dcf61a52625165ba71c9797c86b9d3d48f3c8a0
Michael Tulskikh <mtulskikh@astrabit.io>
2026-01-17 02:42:42 +0200

    Initial commit

---
+++ app/backend/server.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const app = express();
+
+app.get("/health", (_, res) => res.json({ ok: true }));
+app.get("/", (_, res) => res.send("Aequitas Inventory API"));
+
+app.listen(3000, () => console.log("Listening on :3000"));
